b42913502b538c6788f98fbd900791e35c0f72f3
WorkerOfSecret <quan181219@gmail.com>
2017-11-04 13:46:38 +0900

    add

---
+++ python/MyPython/easy_python/input.py
@@ -0,0 +1,14 @@
+##############################
+# File Name: input.py        #
+# Date: 7/2017               #
+# Author: Nguyen Anh Quan    #
+##############################
+
+
+##############################################
+def main():
+  something = input('try something: ')
+  print something, 'is what I want'
+
+##############################################
+main()
