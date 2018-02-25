5563552c9022224a12adc5d81c3cd6dd931aecb8
WorkerOfSecret <s17c1054bz@s.chibakoudai.jp>
2018-02-25 17:23:12 +0900

    new changes

--- python/my_python/test/class/my_class.py
+++ python/my_python/test/class/my_class.py
@@ -1,4 +1,4 @@
-class Monster:
+class Monster(object):
     def __init__(self, lv, hp, dfd, atk = 1):
         self.lv = lv
         self.hp = self.lv * hp
@@ -9,11 +9,11 @@ class Monster:
         self.status()
     
     def status(self):
-        print 'LV:', self.lv
-        print 'HP:', self.hp
-        print 'ATK:', self.atk
-        print 'DF:', self.dfd
-        print 'COLOR:', self.color
+        print ('LV:', self.lv)
+        print ('HP:', self.hp)
+        print ('ATK:', self.atk)
+        print ('DF:', self.dfd)
+        print ('COLOR:', self.color)
     
     def rage(self):
         self.atk *= 2
@@ -26,11 +26,36 @@ class Monster:
         self.color = 'green'
 
 
+class Alien(Monster):
+    # When the child class's function has the same name with the parent class's
+    #function the child's one will replace the parent's one, use super() to call
+    #the parent's one
+    # Super cannot use with old style class in python 2 that why if you call out
+    #a class like class Monster:, or class Moster(): python will give you an error
+    def __init__(self, lv, hp, dfd, atk):
+        Monster.__init__(self,lv, hp, dfd, atk)
+        Monster.tired(self)
+        super(Alien, self).status()
+        try:
+            # This is how you use super() in python3 to compile use python3 
+            # file_name.py
+            super().__init__(lv,hp,dfd,atk)
+        except Exception:
+            pass
+    def call(self):
+        print ("I'm an alien, Woohoo")
+
+
+class
+
 
 def main():
     slime = Monster(1, 5, 2)
-    slime.rage()
-    slime.status()
+    
+    predator = Alien(10, 100, 40, 100)
+    # Just want to let you know that when python run the class what will the self
+    #be
+    Alien.call(predator)
 
 
 if __name__ == '__main__':
