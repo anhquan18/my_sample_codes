6869cb6f9c137be371703382fe41434dec7796de
WorkerOfSecret <quan181219.@gmail.com>
2018-04-11 22:10:58 +0900

    changed

--- linux_shell_blah_blah/shell/test/try_for.sh
+++ linux_shell_blah_blah/shell/test/try_for.sh
@@ -5,10 +5,10 @@ do
     echo $num
 done
 
-for num in $(seq 10 100)
-do
-    touch "0${num}.txt"
-done
+#for num in $(seq 10 100)
+#do
+#    touch "0${num}.txt"
+#done
 
 homesize
 echo "exit status $?"
