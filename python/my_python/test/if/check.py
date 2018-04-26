c9064d3a4151d41180ff1793fa83384bb04cc8ff
WorkerOfSecret <s17c1054bz@s.chibakoudai.jp>
2018-04-26 21:41:50 +0900

    updated

---
+++ python/my_python/test/if/check.py
@@ -0,0 +1,7 @@
+import sys
+
+none, a, b = sys.argv
+print sys.argv
+print a,b
+if a == 1:
+    print 'Yay'
--- python/my_python/test/if/if_one_line.py
+++ python/my_python/test/if/if_one_line.py
@@ -1,5 +1,5 @@
 a = 0
 b = input('Input: ')
-a = 1 if b == 1 else False
+a = 1 if b == 1 or b is 20 else False
 print a
 print 'Yay' if a == 1 else 'Bye'
