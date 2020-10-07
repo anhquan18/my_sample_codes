8ea4568c68b40657064761fd7c2e816e5657703f
anhquan18 <s17c1054bz@s.chibakoudai.jp>
2020-10-07 16:12:55 +0900

    change directory name from test to sample code

--- robocup/src/function_of_SoccerPlayer.py
+++
@@ -1,7 +0,0 @@
-    def wait_until_status_updated():
-        print '''1. Call out the wait_until_status_updated from SoccerAgent.brain which will return the one from src/HPL/hpl_base.cpp 
-                    (I think this will sleep a few milisecond not sure after that)
-                 2. Call out the self.exploration from SoccerAgent.brain which will get_self_pos and some time(don't know)
-                 3. Update self strategy and it will return True or False
-                 4. From the T or F the Error not changed will be raised'''
-
