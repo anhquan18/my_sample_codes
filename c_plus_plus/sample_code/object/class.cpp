cda941805fb472b5f7377338800259aa5affa442
anhquan18 <s17c1054bz@s.chibakoudai.jp>
2020-12-09 19:41:58 +0900

    add parametrized, copy, default constructor sample and documents

--- c_plus_plus/sample_code/object/class.cpp
+++ c_plus_plus/sample_code/object/class.cpp
@@ -34,7 +34,28 @@
  *      int show();
  *  };
  *
- * ######## function inside class will be defined outside class and connect to the class by "::(scope resolution operator)" . With this the speed of calling 
- * ######## the function and return value will be a little bit slower but the program size will be smaller, if you define function inside class it will be faster but 
- * ######## the program will be longer. And there is inline that will help that when the compiler run the function attach to inline (being short enough) will be 
- * ######## considered as defined inside a class and have lower cost when you call out the function "inline type ClassName::functionName(argument) {...}"
+ * ### function inside class will be defined outside class and connect to the class by "::(scope resolution operator)" 
+ *
+ * ### With this the speed of calling the function and return value will be a little bit slower 
+ * but the program size will be smaller, if you define function inside class it will be faster 
+ * but the program will be longer 
+ *
+ * ### Inline function will help the compiler run the function attach to inline (being short enough) 
+ * will be considered as defined inside a class and have lower cost when you call out the function 
+ * Ex: "inline type ClassName::functionName(argument) {...};"
+ *
+ * ### Dot('.') operator access the members of plain data structures
+ * Ex: "class_name.member_name(member function name);"
+ *
+ * ### Constructor: special class members called by an constructor every time an object of that class is isntantiated.
+ * Constructors have the same name as the class and may be defined outside or inside the class definition.
+ * There are 3 types of constructors: Default, Parametrized, Copy
+ * Copy:a class member function which intializes an object using another object of the same class
+ * Copy Constructor is called in following cases:
+ * 1.When an object of the class is returned by value
+ * 2.When an object of the class is passed(to a function) by values as an argument
+ * 3.When an object is constructed based on another object of the same class
+ * 4.When the compiler generates a temporary object
+ * However there are certain cases Copy Constructor is not called(Ex: https://en.wikipedia.org/wiki/Copy_elision#Return_value_optimization)
+ *
+ * ### Destructors
--- c_plus_plus/sample_code/object/sample1.cpp
+++ c_plus_plus/sample_code/object/sample1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 class MyClass 
@@ -6,15 +7,50 @@ class MyClass
     private:
         int num1;
     public:
+        int id = 10;
         int num2;
-        int set_num();
-        int show_num();
+        void set_num();
+        void show_num();
+        //void printname();
+        void show_name();
+        string name = "My class";
+
+        //Default constructor
+        MyClass()
+        {
+            cout << "Default constructor called" << endl;
+            id -= 1;
+            cout << id << endl;
+        }
+        
+        //Parametrized Constructor
+        MyClass(int num)
+        {
+            cout << "Parametrized constructor called" << endl;
+            id = num;
+            cout << id << endl;
+        }
+
+        //Copy constructor
+        MyClass(const MyClass &old_class) {id = old_class.id; name = old_class.name;}
+        //void printname()
+        {
+            cout << "Class name is: " << name << endl;
+        }
+
+    protected:
+        int num3;
 };
+
+inline void MyClass::show_name()
+{
+    cout << name << endl;
+}
 
 void MyClass::set_num()
 {
     cout << "Input num: ";
-    cin >> num1;
+    cin >> num1; // access member without using self like python
 }
 
 void MyClass::show_num()
@@ -24,7 +60,13 @@ void MyClass::show_num()
 
 int main()
 {
-    MyClass mc;
+    cout << "main function" << endl;
+    MyClass mc; //mc will call Default Constructor
+    MyClass mc2(200); //mc2 will call Parametrized Constructor
+    MyClass mc3 = mc; //Copy Constructor called
+    //mc2 = mc1 Assignment constructor
 
     mc.num2 = 100; // You can set 
+    mc.printname();
+    mc3.printname();
 }
